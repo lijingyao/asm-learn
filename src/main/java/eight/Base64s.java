f3dfcb2a45ba658cb74d19531a04288d12c1df5a
lijingyao <lijingyao8206@163.com>
2015-09-28 21:14:02 +0800

    java8 -base64

---
+++ src/main/java/eight/Base64s.java
@@ -0,0 +1,23 @@
+package eight;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+/**
+ * 编码解码，url、字符串、mime等
+ * Created by yunshen.ljy on 2015/7/28.
+ */
+public class Base64s {
+    // （Base64.getUrlEncoder() / Base64.getUrlDecoder(), Base64.getMimeEncoder()
+    // / Base64.getMimeDecoder()）。
+
+    public static void main(String[] args) {
+        final String text = "さよならにさよなら";
+
+        final String encoded = Base64.getEncoder().encodeToString(text.getBytes(StandardCharsets.UTF_8));
+        System.out.println(encoded);
+
+        final String decoded = new String(Base64.getDecoder().decode(encoded), StandardCharsets.UTF_8);
+        System.out.println(decoded);
+    }
+}
